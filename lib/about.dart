11640ff51903c52ac47d17cce2ffb76ce88a817c
Mel Macario <124644863+Kyusans@users.noreply.github.com>
2023-08-21 09:57:17 +0800

    Update about.dart

--- lib/about.dart
+++ lib/about.dart
@@ -15,7 +15,48 @@ class _AboutState extends State<About> {
         title: const Text('About'),
         centerTitle: true,
       ),
-      body: Container(),
+      body: Container(
+        padding: const EdgeInsets.all(16),
+        child: const Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Welcome to my contact app! practice ra bitaw ni sa flutter hehehehe",
+              style: TextStyle(fontSize: 16),
+            ),
+            SizedBox(height: 24),
+            Text(
+              "Developer:",
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text("Mel Macario"),
+            Text("Email: xmelmacario@gmail.com"),
+            SizedBox(height: 16),
+            Text(
+              "Features",
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text('• Add, edit, and delete contacts'),
+            Text('• Search for contacts by name or phone number'),
+            SizedBox(height: 16),
+            Text(
+              "Contact u̶s̶ me",
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text('If you have any questions, feedback, or support requests,'),
+            Text('please email u̶s̶ me at xmelmacario@gmail.com'),
+          ],
+        ),
+      ),
     );
   }
 }
